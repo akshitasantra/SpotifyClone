eadbba4a7b09b4eb90953829e7dcbf50a6803929
akshitasantra <106004567+akshitasantra@users.noreply.github.com>
2025-06-15 21:00:17 -0500

    Album Screen - Song List (FlatList)

--- app/AlbumScreen.tsx
+++ app/AlbumScreen.tsx
@@ -1,39 +1,11 @@
 import React, {useEffect} from 'react';
-import { View, Text } from 'react-native';
+import {View, Text, FlatList} from 'react-native';
 import { useRoute } from '@react-navigation/native';
 
-const album = {
-    id: '11',
-    name: 'Good vibes',
-    by: 'Spotify',
-    numberOfLikes: 38,
-    imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
-    artistsHeadline: 'Taylor Swift, Kygo Objective C, Avicii',
-    songs: [{
-        id:'1',
-        imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
-        title: 'High on You',
-        artist: 'Helen',
-    },
-    {
-        id:'2',
-        imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
-        title: 'Lorem Ipsum',
-        artist: 'Helen',
-    },
-    {
-        id:'3',
-        imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
-        title: 'Hello from the other side',
-        artist: 'Helen',
-    },
-    {
-        id:'4',
-        imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
-        title: 'High on You',
-        artist: 'Helen',
-    }]
-}
+import albumDetails from '../data/albumDetails'
+import SongListItem from '../components/SongListItem';
+import AlbumComponent from "@/components/Album";
+
 const AlbumScreen = () => {
     const route = useRoute();
 
@@ -43,7 +15,11 @@ const AlbumScreen = () => {
 
     return (
         <View>
-            <Text style={{color: 'white'}}>Hello from Album Screen</Text>
+            <FlatList
+                data={albumDetails.songs}
+                renderItem={({ item }) => <SongListItem song={item} />}
+                keyExtractor={( item ) => item.id}
+            />
         </View>
     )
 }
